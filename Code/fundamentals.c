52cd094ba6581176387fffbfa08cc9b322c806e1
Sanghyeon111 <105565116+Sanghyeon111@users.noreply.github.com>
2022-08-12 02:32:56 -0400

    Update fundamentals.c

--- Code/fundamentals.c
+++ Code/fundamentals.c
@@ -28,4 +28,35 @@ void fundamentals(void) {		// This function is to collect string from user and f
 		}
 	} while (strcmp(buffer1, "q") != 0);			// Terminates iteration when user enter 'q'
 	printf("*** End of Indexing Strings Demo ***\n\n");
+	
+	//V2
+	printf("*** Start of Measuring Strings Demo ***\n");
+	char buffer2[BUFFER_SIZE];		// Declare char array and store string from user
+	do {
+		printf("Type a string (q - to quit):\n");
+		fgets(buffer2, BUFFER_SIZE, stdin);		//Receive char from user and store in buffer2
+		buffer2[strlen(buffer2)-1] = '\0';	// Assign final char as null character
+		if (strcmp(buffer2, "q") != 0);			// This condition executes only if user does input 'q'
+		printf("The length of \'%s\' is %d characters\n",	//display numbers of char that user entered
+				buffer2, (int)strlen(buffer2));
+	} while (strcmp(buffer2, "q") != 0);		//Terminate iteration when user enter 'q'
+	printf("*** End of Measuring Strings Demo ***\n\n");
+
+	//V3
+	printf("*** Start of copying Strings Demo ***\n");
+	char destination[BUFFER_SIZE]; // Declare char array
+	char source[BUFFER_SIZE];		// Declare char array from user
+	do {
+		destination[0] = '\0';		//declare first index of destination array is NULL
+		printf("Destination string is reset to empty\n");
+		printf("Type the source string (q - to quit):\n");
+		fgets(source, BUFFER_SIZE, stdin);	//Receive char from user and store in source
+		source[strlen(source) - 1] = '\0';	//Assign final char as null character
+		if (strcmp(source, "q") != 0)		// This condition executes only if user does input 'q'
+		{
+			strcpy(destination, source); // copy string in source array to destination array
+			printf("New destination string is \'%s\'\n", destination); //Display string in destination
+		}
+	} while (strcmp(source, "q") != 0); //Terminate iteration when user enter 'q'
+	printf("*** End of Copying Strings Demo ***\n\n");
 }
